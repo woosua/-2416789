f2f46479f80e828e81d175060e5c4fce7c0afa51
woosua <tndk24@sookmyung.ac.kr>
2024-09-25 00:49:40 +0900

    week02-2

---
+++ week02-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	int a, b;
+	
+	printf("Hello, Electronics!\n");
+	a=10;
+	b=20;
+	
+	printf("%d+%d=%d\n", a, b, a+b);
+	return 0;
+}
