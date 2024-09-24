f6cef736d3de12afc8515f00e759284ff7d9f6fd
woosua <tndk24@sookmyung.ac.kr>
2024-09-25 01:15:37 +0900

    week02-3

--- week02-3.c
+++ week02-3.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+	int a, b;
+	
+	
 	printf("Hello, Sookmyung Electronics!\n");
+	a=10;
+	b=20;
+	
+	printf("%d+%d=%d\n", a, b, a+b);
 	return 0;
 }
