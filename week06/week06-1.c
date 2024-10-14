aa52f79cfb62a469604d28aa02b62c36a29317f4
woosua <tndk24@sookmyung.ac.kr>
2024-10-14 13:13:19 +0900

    week06-1

---
+++ week06/week06-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x;
+	printf("Enter an integer:");
+	scanf("%i",&x);
+	
+	if(x>0){
+		printf("This is positive number.");
+	}
+	else if(x<0){
+		printf("This is negative number.");
+	}
+	else{
+		printf("This is 0.");
+	}
+	return 0;
+}
