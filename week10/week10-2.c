018a467024e93b3a51e1b4feeab5c5cbae35686a
woosua <tndk24@sookmyung.ac.kr>
2024-11-06 13:50:53 +0900

    week10-2

--- week10/week10-2.c
+++ week10/week10-2.c
@@ -4,16 +4,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int i;
+int main(int argc, char *argv[]) {
+	int i,average;
+	int sum;
 	int grade[SIZE];
-	printf("input 5 scores.\n");
 	
-	for(i=10;i<SIZE;i++)
-		scanf("%d",&grade[i]);
-		
+	sum=0;
 	for(i=0;i<SIZE;i++)
-		printf("grade[%d]=%d\n",i,grade[i]);
-		
+	{
+		printf("Enter student grades:");
+		scanf("%d",&grade[i]);
+		sum+=grade[i];
+	}
+	average=sum/SIZE;
+	printf("Grade average:%d\n",average);
 	return 0;
 }
